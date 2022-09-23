843217a43270ec7a3f8cb13c228e3a357c58129f
Guilherme <guilhermeg1703@hotmail.com>
2022-09-23 20:56:01 -0300

    new-feature

--- pages/index.jsx
+++ pages/index.jsx
@@ -18,21 +18,21 @@ export default function Home() {
   
                   </div>
                   <div>
-                      <img src="/2.jpg" alt="image2" />
+                      <img src="/1.png" alt="image2" />
   
                   </div>
                   <div>
-                      <img src="/3.jpg" alt="image3"/>
+                      <img src="/1.png" alt="image3"/>
                       <p className="legend">Image 3</p>
   
                   </div>
                   <div>
-                      <img src="/4.png" alt="image4"/>
+                      <img src="/1.png" alt="image4"/>
                       <p className="legend">Image 4</p>
   
                   </div>
                   <div>
-                      <img src="/5.jpg" alt="image5"/>
+                      <img src="/1.png" alt="image5"/>
                       <p className="legend">Image 5</p>
   
                   </div>
@@ -40,27 +40,34 @@ export default function Home() {
       <div>
 
       </div>
-      <Container className="">
+      <Container className="containerBanner" fluid>
         <Row>
           <Col>
             <Image src={test} alt="teste"/>
           </Col>
-          <Col >
+          <Col className="pt-5">
                 <p className="textMovie "> Produzimos as melhores embalagens personalizadas que sua empresa merece, tudo com rapidez e qualidade</p>
-                <Button className="">Solicite um Orçamento</Button>
+                <button className="btnVI fundoBtnBanner">Solicite um Orçamento</button>
           </Col>
         </Row>
       </Container>
-      <Container >
-          <Row>
+      <Container className="text-center pt-5 pb-2">
+        <Row className="pb-3">
             <p>Acreditamos que a primeira impressão é a que fica, então a ABA Embalagens oferece a melhor embalagem para o seu produto, personalizada do seu jeito!</p>
-            <p>lugar da image</p>
+        </Row >
+        <Row>
+        <iframe className="" width="727" height="409" src="https://www.youtube.com/embed/YTTvB8yQDt8" title="Vídeo Institucional Aba" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+        </Row>
+        <Row className="pt-5">
         <p>Produzimos as melhores embalagens personalizadas que sua empresa merece, tudo com rapidez e qualidade</p>
-        <p><button>Confira nossos produtos!</button></p>
-          </Row >
-          </Container>
+        </Row>
+        <Row className="displayBtn">
+        <button className="btnVI widthBtnVI fundoBtnText">Confira nossos produtos!</button>
+        </Row>
+
+      </Container>
           <Container >
-            <Row xs="6" className="pt-5 justify-content-around">
+            <Row xs="6" className="pt-5 justify-content-around text-center">
             <Col className="">
                 <Image src="/i-pers-r.png" alt="" width="150" height="150" layout="intrinsic"/>
                 <p>Personalização do seu jeito!</p>
@@ -74,8 +81,8 @@ export default function Home() {
             <p>Entrega Rápida</p>
             </Col>
             <Col className="testevh">
-            <Image src="/i-entrega-r.png" alt="" width="150" height="150" layout="intrinsic"/>
-            <p>Entrega Rápida</p>
+            <Image src="/i-timer-r.png" alt="" width="150" height="150" layout="intrinsic"/>
+            <p>Produção Rápida</p>
             </Col>
             </Row>
       </Container>
