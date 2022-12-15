6a3d3d1aebe83ad84d8366fd771686c3ef79a988
Guilherme <guilhermeg1703@hotmail.com>
2022-12-15 00:48:10 -0300

    responsive adaptation to products

--- pages/produto.jsx
+++ pages/produto.jsx
@@ -14,6 +14,60 @@ export default function Produto () {
             </div>
             <div className="face face2">
                 <div className="content">
+                    <p className="desc-card-hover">• Caixas de Pizza oitavadas, fotográficas e conjugadas. Impressão off set fotográfica .
+Tamanhos: Broto 25, Caixa 30, Caixa 35 e Caixa 40.
+</p>
+                        <a className="card-products-button" href="#">Read More</a>
+                </div>
+            </div>
+        </div>
+        <div className="card">
+            <div className="face face1">
+                <div className="">
+                <Image src={ImageAbout} alt="Modelo Aba" layout='fill'/>
+                </div>
+            </div>
+            <div className="face face2">
+                <div className="content">
+                    <p className="desc-card-hover">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cum cumque minus iste veritatis provident at.</p>
+                        <a className="card-products-button" href="#">Read More</a>
+                </div>
+            </div>
+        </div>
+        <div className="card">
+            <div className="face face1">
+                <div className="">
+                <Image src={ImageAbout} alt="Modelo Aba" layout='fill'/>
+                </div>
+            </div>
+            <div className="face face2">
+                <div className="content">
+                    <p className="desc-card-hover">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cum cumque minus iste veritatis provident at.</p>
+                        <a className="card-products-button" href="#">Read More</a>
+                </div>
+            </div>
+        </div>
+        <div className="card">
+            <div className="face face1">
+                <div className="">
+                <Image src={ImageAbout} alt="Modelo Aba" layout='fill'/>
+                </div>
+            </div>
+            <div className="face face2">
+                <div className="content">
+                    <p className="desc-card-hover">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cum cumque minus iste veritatis provident at.</p>
+                        <a className="card-products-button" href="#">Read More</a>
+                </div>
+            </div>
+        </div>
+        <div className="card">
+            <div className="face face1">
+                <div className="">
+                <Image src={ImageAbout} alt="Modelo Aba" layout='fill'/>
+                </div>
+            </div>
+            <div className="face face2">
+                <div className="content">
                     <p className="desc-card-hover">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cum cumque minus iste veritatis provident at.</p>
                         <a className="card-products-button" href="#">Read More</a>
                 </div>
