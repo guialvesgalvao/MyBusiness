b82132453b4ceedb2c85b40cc211e7a7811eed4f
Guilherme <guilhermeg1703@hotmail.com>
2023-02-01 01:19:01 -0300

    deploy netlify debug test #2

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,15 @@
-import { UncontrolledCarousel,Button,Row,Col, Container } from "reactstrap";
+import React from 'react';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+import { Row, Col, Container } from "reactstrap";
+import { Carousel } from 'react-responsive-carousel';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faWhatsapp} from '@fortawesome/free-brands-svg-icons';
-import React, { Component } from 'react';
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { Carousel } from 'react-responsive-carousel';
-import test from '../public/faixaBanner.png'
 
 import Image from "next/image";
+import embalagensApst from '../public/faixaBanner.png';
+
 
 export default function Home() {
   
@@ -27,56 +30,104 @@ export default function Home() {
                   </div>
               </Carousel>
       <div>
-
       </div>
       <Container className="containerBanner" fluid>
         <Row>
           <Col>
-            <Image src={test} alt="teste"/>
+            <Image src={embalagensApst} alt="Embalagens Diversas"/>
           </Col>
           <Col className="pt-5">
-                <p className="textMovie "> Produzimos as melhores embalagens personalizadas que sua empresa merece, tudo com rapidez e qualidade</p>
-                <button className="btnVII fundoBtnBanner">Solicite um Orçamento</button>
+                <p className="textMovie">
+                  Produzimos as melhores embalagens personalizadas que sua empresa merece, tudo com rapidez e qualidade
+                </p>
+                <button className="btnVII fundoBtnBanner">
+                  Solicite um Orçamento
+                </button>
           </Col>
         </Row>
       </Container>
       <Container className="text-center pt-5 pb-2 fontQuick">
         <Row className="pb-3 pStyle">
-            <p>Acreditamos que a primeira impressão é a que fica, então a ABA Embalagens oferece a melhor embalagem para o seu produto, personalizada do seu jeito!</p>
+            <p>
+              Acreditamos que a primeira impressão é a que fica, então a ABA Embalagens oferece a melhor embalagem para o seu produto, personalizada do seu jeito!
+            </p>
         </Row >
         <Row>
-        <iframe className="" width="727" height="409" src="https://www.youtube.com/embed/YTTvB8yQDt8" title="Vídeo Institucional Aba" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+          <iframe 
+          width="727"
+          height="409"
+          src="https://www.youtube.com/embed/YTTvB8yQDt8"
+          title="Vídeo Institucional Aba"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen>
+          </iframe>
         </Row>
         <Row className="pt-5 pStyle ab">
-        <p>Produzimos as melhores embalagens personalizadas que sua empresa merece, tudo com rapidez e qualidade</p>
+        <p>
+          Produzimos as melhores embalagens personalizadas que sua empresa merece, tudo com rapidez e qualidade
+        </p>
         </Row>
         <Row className="displayBtn">
-        <button className="btnVI widthBtnVI fundoBtnText">Confira nossos produtos!</button>
+        <button className="btnVI widthBtnVI fundoBtnText">
+          Confira nossos produtos!
+        </button>
         </Row>
 
       </Container>
           <Container >
             <Row xs="6" className="pt-5 justify-content-around text-center fontQuick">
-            <Col className="">
-                <Image src="/i-pers-r.png" alt="" width="150" height="150" layout="intrinsic"/>
-                <p className="descSty">Personalização do seu jeito!</p>
+            <Col>
+              <Image
+              src="/i-pers-r.png"
+              alt=""
+              width="150"
+              height="150"
+              layout="intrinsic"/>
+              <p className="descSty">
+                Personalização do seu jeito!
+              </p>
             </Col>
-            <Col className="">
-            <Image src="/i-caixa-r.png" alt="" width="150" height="150" layout="intrinsic"/>
-            <p className="descSty">Resistência e Qualidade</p>
+            <Col>
+              <Image
+              src="/i-caixa-r.png"
+              alt=""
+              width="150"
+              height="150"
+              layout="intrinsic"/>
+              <p className="descSty">
+                Resistência e Qualidade
+              </p>
             </Col>
-            <Col className="">
-            <Image src="/i-entrega-r.png" alt="" width="150" height="150" layout="intrinsic"/>
-            <p className="descSty">Entrega Rápida</p>
+            <Col>
+              <Image
+              src="/i-entrega-r.png"
+              alt=""
+              width="150"
+              height="150"
+              layout="intrinsic"/>
+              <p className="descSty">
+                Entrega Rápida
+              </p>
             </Col>
-            <Col className="">
-            <Image src="/i-timer-r.png" alt="" width="150" height="150" layout="intrinsic"/>
-            <p className="descSty">Produção Rápida</p>
+            <Col>
+              <Image
+              src="/i-timer-r.png"
+              alt=""
+              width="150"
+              height="150"
+              layout="intrinsic"/>
+              <p className="descSty">
+                Produção Rápida
+              </p>
             </Col>
             </Row>
       </Container>
       <div className="popup">
-        <a href="https://api.whatsapp.com/send?1=pt_BR&phone=5511995249688" className="whatsapp-link" target="_blank" rel="noreferrer">
+        <a 
+        href="https://api.whatsapp.com/send?1=pt_BR&phone=5511995249688"
+        className="whatsapp-link"
+        target="_blank"
+        rel="noreferrer">
         <FontAwesomeIcon icon={faWhatsapp} />
         </a>
       </div>
--- pages/todos-produtos.jsx
+++ pages/todos-produtos.jsx
@@ -1,4 +1,3 @@
-import ImageAbout from '../public/12.png';
 import i21 from '../public/21.png';
 import i22 from '../public/22.png';
 import i23 from '../public/23.png';
@@ -19,92 +18,155 @@ export default function Produto () {
         <div className="card">
             <div className="face face1">
                 <div className="">
-                <Image src={i29} alt="Caixas de Pizza" layout='fill'/>
+                <Image
+                src={i29}
+                alt="Caixas de Pizza"
+                layout='fill'/>
                 </div>
             </div>
             <div className="face face2">
                 <div className="content">
-                    <p className="desc-card-hover">• Caixas de Pizza oitavadas, fotográficas e conjugadas. Impressão off set fotográfica .
-                    Tamanhos: Broto 25, Caixa 30, Caixa 35 e Caixa 40.</p>
-                    <Link href="/produtos/caixas-de-pizza"><a className="card-products-button" href="">Ver Mais</a></Link>
+                    <p className="desc-card-hover">
+                    • Caixas de Pizza oitavadas, fotográficas e conjugadas. Impressão off set fotográfica .
+                    Tamanhos: Broto 25, Caixa 30, Caixa 35 e Caixa 40.
+                    </p>
+                    <Link href="/produtos/caixas-de-pizza">
+                        <a className="card-products-button" href="">
+                            Ver Mais
+                        </a>
+                    </Link>
                 </div>
             </div>
         </div>
         <div className="card">
             <div className="face face1">
                 <div className="">
-                <Image src={i21} alt="Guardanapos Personalizados" layout='fill'/>
+                <Image
+                src={i21}
+                alt="Guardanapos Personalizados"
+                layout='fill'/>
                 </div>
             </div>
             <div className="face face2">
                 <div className="content">
-                    <p className="desc-card-hover">Guardanapos personalizados em diferentes tamanhos.</p>
-                    <Link href="/produtos/guardanapos"><a className="card-products-button" href="#">Ver Mais</a></Link>
+                    <p className="desc-card-hover">
+                        Guardanapos personalizados em diferentes tamanhos.
+                    </p>
+                    <Link href="/produtos/guardanapos">
+                        <a className="card-products-button" href="#">
+                            Ver Mais
+                        </a>
+                    </Link>
                 </div>
             </div>
         </div>
         <div className="card">
             <div className="face face1">
                 <div className="">
-                <Image src={i22} alt="Copos Personalizados" layout='fill'/>
+                <Image
+                src={i22}
+                alt="Copos Personalizados"
+                layout='fill'/>
                 </div>
             </div>
             <div className="face face2">
                 <div className="content">
-                    <p className="desc-card-hover">Trabalhamos com diversos tipos de Copos Personalizados em diferentes tamanhos.</p>
-                    <Link href="/produtos/copos"><a className="card-products-button" href="#">Ver Mais</a></Link>
+                    <p className="desc-card-hover">
+                        Trabalhamos com diversos tipos de Copos Personalizados em diferentes tamanhos.
+                    </p>
+                    <Link href="/produtos/copos">
+                        <a className="card-products-button" href="#">
+                            Ver Mais
+                        </a>
+                    </Link>
                 </div>
             </div>
         </div>
         <div className="card">
             <div className="face face1">
                 <div className="">
-                <Image src={i23} alt="Sacos de Papel" layout='fill'/>
+                <Image
+                src={i23}
+                alt="Sacos de Papel"
+                layout='fill'/>
                 </div>
             </div>
             <div className="face face2">
                 <div className="content">
-                    <p className="desc-card-hover">Trabalhamos com diversos tipos de sacos personalizados em diferentes tamanhos.</p>
-                    <Link href="/produtos/sacos-de-papel"><a className="card-products-button" href="#">Ver Mais</a></Link>
+                    <p className="desc-card-hover">
+                        Trabalhamos com diversos tipos de sacos personalizados em diferentes tamanhos.
+                    </p>
+                    <Link href="/produtos/sacos-de-papel">
+                        <a className="card-products-button" href="#">
+                            Ver Mais
+                        </a>
+                    </Link>
                 </div>
             </div>
         </div>
         <div className="card">
             <div className="face face1">
                 <div className="">
-                <Image src={i24} alt="Caixas E-Commerce" layout='fill'/>
+                <Image
+                src={i24}
+                alt="Caixas E-Commerce"
+                layout='fill'/>
                 </div>
             </div>
             <div className="face face2">
                 <div className="content">
-                    <p className="desc-card-hover">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cum cumque minus iste veritatis provident at.</p>
-                    <Link href="/produtos/caixas-ecommerce"><a className="card-products-button" href="#">Ver Mais</a></Link>
+                    <p className="desc-card-hover">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cum cumque minus iste veritatis provident at.
+                    </p>
+                    <Link href="/produtos/caixas-ecommerce">
+                        <a className="card-products-button" href="#">
+                            Ver Mais
+                        </a>
+                    </Link>
                 </div>
             </div>
         </div>
         <div className="card">
             <div className="face face1">
                 <div className="">
-                <Image src={i25} alt="Impressos Gráficos" layout='fill'/>
+                <Image
+                src={i25}
+                alt="Impressos Gráficos"
+                layout='fill'/>
                 </div>
             </div>
             <div className="face face2">
                 <div className="content">
-                    <p className="desc-card-hover">Catálogos, folhetos e papelaria.</p>
-                    <Link href="/produtos/impressos-graficos"><a className="card-products-button" href="#">Ver Mais</a></Link>
+                    <p className="desc-card-hover">
+                        Catálogos, folhetos e papelaria.
+                    </p>
+                    <Link href="/produtos/impressos-graficos">
+                        <a className="card-products-button" href="#">
+                            Ver Mais
+                        </a>
+                    </Link>
                 </div>
             </div>
         </div>
         <div className="card">
             <div className="face face1">
                 <div className="">
-                <Image src={i26} alt="Pdv" layout='fill'/>
+                <Image
+                src={i26}
+                alt="PDV"
+                layout='fill'/>
                 </div>
             </div>
             <div className="face face2">
                 <div className="content">
-                    <p className="desc-card-hover">Sua empresa em destaque nos pontos de venda.</p>
-                    <Link href="/produtos/pdv"><a className="card-products-button" href="#">Ver Mais</a></Link>
+                    <p className="desc-card-hover">
+                        Sua empresa em destaque nos pontos de venda.
+                    </p>
+                    <Link href="/produtos/pdv">
+                        <a className="card-products-button" href="#">
+                            Ver Mais
+                        </a>
+                    </Link>
                 </div>
             </div>
         </div>
